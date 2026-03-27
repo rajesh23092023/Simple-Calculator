141509b0cbcc0a1bf03b7ad9e1d0ce2fffd94c0f
Rajesh Patro <rajeshpatro1001@gmail.com>
2026-03-28 01:00:47 +0530

    Add files via upload

---
+++ caculator.py
@@ -0,0 +1,33 @@
+def calculator():
+    print("--- Simple Calculator ---")
+    
+    while True:
+        print("Select operation:")
+        print("1. Add      2. Subtract")
+        print("3. Multiply 4. Divide")
+        print("5. Exit")
+
+        choice = input("Enter choice (1/2/3/4/5): ")
+        if choice == '1':
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result = ", num1 + num2)
+        elif choice == '2':
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result = ", num1 - num2)
+        elif choice == '3':
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result = ", num1 * num2)
+        elif choice == '4':
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if num2 != 0:
+                print("Result = ", num1 / num2)
+        elif choice == '5':
+            print("Exiting the calculator. Goodbye!")
+        else:
+            print("Invalid input. Please select a valid operation.")
+        break
+calculator()
